ed7398e0183605ab8dad2974ade9df7d25ace45b
joblowow <j.a.blankestijn.1@student.rug.nl>
2019-12-12 12:32:47 +0100

    modify() fixed issues with fact updating. Bug fixes for Cost.java. Still two functions that are incorrect in that file (see file)

--- Sheepherding2/src/main/java/com/sample/Cost.java
+++ Sheepherding2/src/main/java/com/sample/Cost.java
@@ -3,23 +3,26 @@ package com.sample;
 public class Cost {
 	   
 	public static float numberOfSheepMore(float land, int sheep){
-		   	return (float) (land - (sheep * 2.85) * 2.85);
+		   	return (float) (land - (landNeeded(sheep) * 2.85));
 	   }
 	   
-	   public static float shedSize(float shedsize, int sheep){
-		   	return (float) (sheep* 10 - shedsize);
+	   public static float shedSize(int sheep, float shedsize){
+		   	return (float) (sheep * 10 - shedsize);
 	   }
 	   
-	   public static float numberOfAcresMore(float land, int sheep){
-		   	return (float) (sheep * 2.85 - land);
+	   public static float numberOfAcresMore(int sheep, float land){
+		   	return (float) (landNeeded(sheep) - land);
 	   }
-	   
-	   public static float neededMoneyForLand(float land, int sheep){
-		   	return (float)  (sheep * 2.85 - land* 50000);
+	   /* This is wrong */
+	   public static float neededMoneyForLand(int sheep, float land){
+		   	return (float)  (sheep * 2.85 - land * 50000);
 	   }
-	   
-	   public static float toeslagrechten(float land, int sheep){
-		   	return (float)  (sheep * 2.85 - land* 300);
+	   /* This is wrong */
+	   public static float toeslagrechten(int sheep, float land){
+		   	return (float)  (sheep * 2.85 - land * 300);
+	   }
+	   public static float landNeeded(int sheep) {
+		   return (float) (sheep * 2.85);
 	   }
 
 }
